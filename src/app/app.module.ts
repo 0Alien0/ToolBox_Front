09c7626b66b90a2b77d040ea30a5c937e71fabc2
Gavin Lee <sz110010@gmail.com>
2016-10-04 02:16:52 +0800

    Remove nav branch
    Using header replace nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		NavComponent
 	],
 	imports: [
 		BrowserModule,
